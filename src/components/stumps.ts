1f4916e731878adb7046dd5b9c2d72493cbe6f66
Jackson Beard <6334208+Starman10@users.noreply.github.com>
2021-02-19 21:55:19 -0800

    Created Stumpweight but I can't tell if it works
    stump weight maybe

--- src/components/stumps.ts
+++ src/components/stumps.ts
@@ -6,21 +6,36 @@ import { CylinderBuilder } from "@babylonjs/core/Meshes/Builders/cylinderBuilder
 import { BoxBuilder } from "@babylonjs/core/Meshes/Builders/boxBuilder";
 
 var stumpHeight = 5.895;
+var weightHeight = 1.179 ;
 
 export class Stump1{
-    mesh: Mesh;
+    mesh:Mesh;
     scene: Scene;
 
     constructor(name: string, scene: Scene) {
 	   this.scene = scene;
-		//stumps
-		this.mesh = CylinderBuilder.CreateCylinder(name, {height: stumpHeight, diameter:0.3}, scene);
+		//stump
+		const stumpWeight = CylinderBuilder.CreateCylinder("stumpWeight", {height: weightHeight, diameter:0.3}, scene);
+		const stumpReal = CylinderBuilder.CreateCylinder("stumpTop", {height: stumpHeight, diameter:0.3}, scene);
+		this.mesh = new Mesh(name, scene);
+        this.mesh.addChild(stumpReal);
+        this.mesh.addChild(stumpWeight);
+		 stumpReal.position.y=1.5;
+		stumpWeight.position.y=0.8;
+		
+		stumpWeight.physicsImpostor = new PhysicsImpostor(stumpWeight, PhysicsImpostor.CylinderImpostor, { mass:20 , restitution: 0.1 }, scene);
 
+		stumpReal.physicsImpostor = new PhysicsImpostor(stumpReal, PhysicsImpostor.CylinderImpostor, { mass: 3, restitution: 0.1 }, scene);
+		
+		
+		//this.mesh = Mesh.MergeMeshes([stumpReal,stumpWeight])!;
+
+		this.mesh.physicsImpostor = new PhysicsImpostor(this.mesh, PhysicsImpostor.CylinderImpostor, { mass: 3, restitution: 0.1 }, scene);
+		
 		this.mesh.position.x = 9;
 		this.mesh.position.y = 1.5;
 		this.mesh.position.z =  -0.6;
-	
-		this.mesh.physicsImpostor = new PhysicsImpostor(this.mesh, PhysicsImpostor.CylinderImpostor, { mass: 3, restitution: 0.1 }, scene);
+		
     }
     
 }
